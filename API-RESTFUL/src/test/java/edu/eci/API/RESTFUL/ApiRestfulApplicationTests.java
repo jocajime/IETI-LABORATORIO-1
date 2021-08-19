38a17a12e3fe3063de5a7b3b0dc046065e0fd760
joel carvajal <joel.carvajal@mail.escuelaing.edu.co>
2021-08-19 17:29:53 -0500

    inicio, Parte 1 punto 1

---
+++ API-RESTFUL/src/test/java/edu/eci/API/RESTFUL/ApiRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.API.RESTFUL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
